c5a96fb2e81c25ff396c85035fe4ecc4bc333013
arshasalimi <arsha.salimi2023@gmail.com>
2025-08-20 11:14:44 +0330

    refactor Nav search

---
+++ src/Pages/HomePage-nav-search.jsx
@@ -0,0 +1,7 @@
+export default function NavSearch() {
+    return (
+        <div className="w-full h-[100vh]">
+
+        </div>
+    );
+}
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,5 @@
+import NavSearch from "./HomePage-nav-search";
+
+export default function HomePage (){
+    <NavSearch/>
+}
