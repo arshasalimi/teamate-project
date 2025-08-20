e7882ccda345d526b81c8428a9d5f704bd68ba5f
arshasalimi <arsha.salimi2023@gmail.com>
2025-08-20 17:11:13 +0330

    complete hero banner

--- src/Pages/HomePage-nav-search.jsx
+++ src/Pages/HomePage-nav-search.jsx
@@ -1,7 +1,18 @@
+import Icon from "../component/ui/Icon/Icon";
+
 export default function NavSearch() {
     return (
-        <div className="w-full h-[100vh]">
-
+        <div>
+            <div className="w-full ">
+                <img src="/Nav-search/Nav-Search.png" className="w-full h-[95vh] relative"></img>
+            </div>
+            <div className="flex justify-center items-center ">
+                <input placeholder="محصول مورد نظر خود را جستجو کنید..." className="rtl top-24 w-[65%] h-[17vh] -translate-y-[60%] rounded-[10px] text-[30px] shadow-xl ">
+                </input>
+                <div className="-translate-y-[10vh] -translate-x-[8vh]">
+                <Icon name={"search"}/>
+                </div>
+            </div>
         </div>
     );
 }
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,8 @@
 import NavSearch from "./HomePage-nav-search";
 
 export default function HomePage (){
+    return(
     <NavSearch/>
+
+    )
 }
