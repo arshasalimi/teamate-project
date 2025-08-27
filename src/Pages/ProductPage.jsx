a6d62cf524ca32e941f790c5aeaf64437f2d47c6
mehrsam <menbguefn48@gmail.com>
2025-08-27 16:15:03 +0330

    complate product page

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -3,16 +3,12 @@ import Icon from "../component/ui/Icon/Icon"
 import ProductsApiPage from "./ProductsApiPage";
 function ProductPage() {
     const [search, setSearch] = useState('');
-    const searchHandler = (value) => {
-        setSearch(value)
-        
-    }
     return (
         <>
 
             <div className="w-full h-[170px] flex justify-center items-center">
                 <div className="w-[77%] h-[49%] flex justify-end bg-gray-400 rounded-[10px]">
-                    <input type="search" placeholder="محصول مورد نظر را جستجو کنید" value={search} onChange={(e) => setSearch(e.target.value)} className="w-[92%] rtl text-[32px] border-none placeholder:text-black outline-none rounded-[10px] text-black bg-gray-400" />
+                    <input type="search" placeholder="محصول مورد نظر را جستجو کنید" value={search} onChange={(e) => setSearch(e.target.value)}  className="w-[92%] rtl text-[32px] border-none placeholder:text-black outline-none rounded-[10px] text-black bg-gray-400" />
                     <div className="w-[8%] flex items-center ml-5">
                         <Icon name="search" />
                     </div>
@@ -33,7 +29,7 @@ function ProductPage() {
                 <hr className="w-[77%] mb-2 border-2 border-[#00000066] rounded-[2px]" />
             </div>
             <div>
-                <ProductsApiPage />
+                <ProductsApiPage search={search} />
             </div>
         </>
     )
